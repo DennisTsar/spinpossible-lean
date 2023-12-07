f2be374441e65ec495dfee245074def3b04725d8
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-12-07 09:08:43 -0500

    Auto-include files

--- lakefile.lean
+++ lakefile.lean
@@ -11,4 +11,5 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib Spinpossible
+lean_lib Spinpossible where
+  globs := #[.submodules `Spinpossible]
