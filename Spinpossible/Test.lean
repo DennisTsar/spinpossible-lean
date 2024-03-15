0143d11360d6d5e35b781537a3c343802f1db7db
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-03-15 02:32:30 -0400

    Cleanup test file
    Now that I semi-know what I'm doing

--- Spinpossible/Test.lean
+++ Spinpossible/Test.lean
@@ -7,93 +7,79 @@ def boardsEqual (b1 b2 : board m n) : Bool := ∀ i j, b1 i j == b2 i j
 
 namespace Basics
 
-  def perm1 : perm 3 :=
-    Equiv.mk
-      (fun i => if i = 0 then 1 else if i = 1 then 0 else 2)
-      (fun i => if i = 0 then 1 else if i = 1 then 0 else 2)
-      (by
-        apply Function.leftInverse_iff_comp.mpr
-        funext i
-        match i with -- I assume this can be shorter, but idk how
-        | 0 => rfl
-        | 1 => rfl
-        | 2 => rfl
-      )
-      (
-        by
-          apply Function.rightInverse_iff_comp.mpr
-          funext i
-          match i with
-          | 0 => rfl
-          | 1 => rfl
-          | 2 => rfl
-      )
+def perm1 : perm 3 where
+  toFun := ![1, 0, 2]
+  invFun := ![1, 0, 2]
+  left_inv := by decide
+  right_inv := by decide
 
-  def perm2 : perm 3 :=
-    Equiv.mk
-      (fun i => i + 1)
-      (fun i => i - 1)
-      (by exact leftInverse_sub_add_left 1)
-      (by exact leftInverse_sub_add_left (-1))
+def perm2 : perm 3 where
+  toFun := fun i => i + 1
+  invFun := fun i => i - 1
+  left_inv := by exact leftInverse_sub_add_left 1
+  right_inv := by exact leftInverse_sub_add_left (-1)
 
-  #guard perm1.toFun = ![1, 0, 2]
-  #guard perm2.toFun = ![1, 2, 0]
-  #guard (perm1 * perm2).toFun = ![2, 1, 0] -- perm1 applied first, then perm2
-  #guard (perm2 * perm1).toFun = ![0, 2, 1] -- perm2 applied first, then perm1
+#guard perm1.toFun = ![1, 0, 2]
+#guard perm2.toFun = ![1, 2, 0]
+#guard (perm1 * perm2).toFun = ![2, 1, 0] -- perm1 applied first, then perm2
+#guard (perm2 * perm1).toFun = ![0, 2, 1] -- perm2 applied first, then perm1
 
 end Basics
 
 namespace TestSpinAction
 
-  def samplePerm : perm (3 * 3) :=
-    Equiv.mk
-      (fun i => if i = 0 then 1 else if i = 1 then 0 else i)
-      (fun i => if i = 0 then 1 else if i = 1 then 0 else i)
-      (by sorry)
-      (by sorry)
+def samplePerm : perm (3 * 3) where
+  toFun := fun i => if i = 0 then 1 else if i = 1 then 0 else i
+  invFun := fun i => if i = 0 then 1 else if i = 1 then 0 else i
+  left_inv := by decide
+  right_inv := by decide
 
-  def sampleSpin : Spin (Nat.toPNat 3) (Nat.toPNat 3) :=
-    {
-      α := samplePerm
-      u := fun i => if i = 0 then 1 else 0
-    }
+def sampleSpin : Spin 3 3 :=
+  {
+    α := samplePerm
+    u := fun i => if i = 0 then 1 else 0
+  }
 
-  def a := Spin.actionOnBoard sampleSpin (Spin.actionOnBoard sampleSpin board3by3)
-  def b := Spin.actionOnBoard (sampleSpin * sampleSpin) board3by3
-  def c := (sampleSpin * sampleSpin).actionOnBoard board3by3
+def samplePerm2 : perm (3 * 3) where
+  toFun := fun i => if i = 4 then 3 else if i = 3 then 1 else if i = 1 then 4 else i
+  invFun := fun i => if i = 4 then 1 else if i = 3 then 4 else if i = 1 then 3 else i
+  left_inv := by decide
+  right_inv := by decide
 
-  #eval a
-  #eval b
-  #eval c
-  #guard boardsEqual a b && boardsEqual a c && boardsEqual b c
+def sampleSpin2 : Spin 3 3 where
+  α := samplePerm2
+  u := fun i => if i % 2 = 0 then 1 else 0
+
+def a := Spin.actionOnBoard sampleSpin (Spin.actionOnBoard sampleSpin2 board3by3)
+def b := (sampleSpin2 * sampleSpin).actionOnBoard board3by3
+
+#eval a
+#eval b
+#guard boardsEqual a b
 
 end TestSpinAction
 
 namespace TestRectSpins
 
-  def test_rectangle : Rectangle (3 : PNat) (3 : PNat) :=
-    {
-      topLeft := {row := 2, col := 0}
-      bottomRight := {row := 2, col:= 2}
-    }
+def test_rectangle : Rectangle 3 3 where
+  topLeft := ⟨2, 0⟩
+  bottomRight := ⟨2, 2⟩
 
-  def test_rectangle2 : Rectangle (3 : PNat) (3 : PNat) :=
-    {
-      topLeft := {row := 0, col := 0}
-      bottomRight := {row := 2, col:= 1}
-    }
+def test_rectangle2 : Rectangle 3 3 where
+  topLeft := ⟨0, 0⟩
+  bottomRight := ⟨2, 1⟩
 
-  def firstSpinRes := performSpin test_rectangle board3by3
-  def secondSpinRes := performSpin test_rectangle2 firstSpinRes
+def firstSpinRes := performSpin test_rectangle board3by3
+def secondSpinRes := performSpin test_rectangle2 firstSpinRes
 
-  def combinedSpin := test_rectangle.toSpin * test_rectangle2.toSpin
-  def combinedSpinRes := combinedSpin.actionOnBoard board3by3
+def combinedSpin := test_rectangle.toSpin * test_rectangle2.toSpin
+def combinedSpinRes := combinedSpin.actionOnBoard board3by3
 
-  #eval firstSpinRes
-  #eval secondSpinRes
-  #eval combinedSpinRes
+#eval firstSpinRes
+#eval secondSpinRes
+#eval combinedSpinRes
 
-  #guard boardsEqual secondSpinRes combinedSpinRes
+#guard boardsEqual secondSpinRes combinedSpinRes
 
 end TestRectSpins
 
