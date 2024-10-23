98e05bf2587a37e828f673e807e6b5a7a0b680b5
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-10-23 12:41:36 -0400

    Finish emptiness proof & cleanup

--- Spinpossible/Prop2.lean
+++ Spinpossible/Prop2.lean
@@ -18,8 +18,7 @@ abbrev Rectangle.fromPoints {m n : PNat} (p1 p2 : Point m n) : Option (Rectangle
 
 -- Define the set of valid spins S(m, n) as a Finset of rectangles
 def validSpins (m n : PNat) : Finset (Rectangle m n) :=
-  Finset.univ ×ˢ (Finset.univ)
-    |>.filterMap (fun (p1, p2) => Rectangle.fromPoints p1 p2) (by aesop)
+  Finset.univ.filterMap (fun (p1, p2) => Rectangle.fromPoints p1 p2) (by aesop)
 
 -- Define the set of spins R(i x j), which are the spins about i x j rectangles
 -- Not sure this is a helpful definition since the return type is too broad
@@ -37,31 +36,43 @@ lemma rectangleSet_cond_iff {r : Rectangle m n} :
   · intro h
     refine Finset.mem_filter.mpr ⟨?_, h⟩
     unfold validSpins
-    simp only [Finset.mem_filterMap, dite_some_none_eq_some, Prod.exists]
+    simp only [Finset.mem_filterMap, Finset.mem_univ, true_and, Prod.exists]
     use r.topLeft, r.bottomRight
-    constructor
-    · apply Finset.mk_mem_product <;> simp only [Finset.mem_univ]
-    · simp only [and_self, exists_const, r.validRow, r.validCol]
+    simp [r.validRow, r.validCol]
 
-/-- The set `R_i×j` is necessarily empty if `i > m` or `j > n` -/
-example {i j m n : PNat} : (i > m ∨ j > n) → (RectangleSet i j m n) = ∅ := by
+/-- "The set `R_i×j` is necessarily empty if `i > m` or `j > n`" -/
+lemma rectangleSet_empty_if {i j m n : PNat} : (i > m ∨ j > n) → (RectangleSet i j m n) = ∅ := by
   intro h
   apply Finset.filter_eq_empty_iff.mpr
   intro r
-  simp_rw [←PNat.coe_lt_coe] at h
+  simp only [←PNat.coe_lt_coe] at h
   omega
 
-/-- we may have `R_i×j = ∅` even when `R_j×i ≠ ∅` (although this can occur only when `m ≠ n`) -/
-example {i j m n} : (RectangleSet i j m n = ∅ ∧ RectangleSet j i m n ≠ ∅) → m ≠ n := by
-  intro ⟨h1, h2⟩
-  have := Finset.filter_eq_empty_iff.mp h1
-  have ⟨r, hr, hp⟩ := Finset.filter_nonempty_iff.mp (Finset.nonempty_iff_ne_empty.mpr h2)
-  specialize this hr
-  have ⟨a, b⟩ := (Finset.mem_filterMap _).mp hr
-  simp [Rectangle.fromPoints] at b
-  obtain ⟨h9, h7, h6⟩ := b
-  simp at *
-  sorry
+/-- "we may have `R_i×j = ∅` even when `R_j×i ≠ ∅` (although this can occur only when `m ≠ n`)" -/
+lemma rectangleSet_empty_nonempty {m n : PNat} :
+  (m ≠ n → ∃ i j, RectangleSet i j m n = ∅ ∧ RectangleSet j i m n ≠ ∅) ∧
+    (RectangleSet i j m n = ∅ ∧ RectangleSet j i m n ≠ ∅ → m ≠ n) := by
+  constructor
+  · intro h
+    use n, m
+    constructor
+    · exact rectangleSet_empty_if (lt_or_gt_of_ne h)
+    · apply Finset.nonempty_iff_ne_empty.mp
+      have : m.val > 0 ∧ n.val > 0 := ⟨m.2, n.2⟩
+      use ⟨⟨⟨0, m.2⟩, ⟨0, n.2⟩⟩, ⟨⟨m - 1, by omega⟩, ⟨n - 1, by omega⟩⟩, by simp, by simp⟩
+      apply rectangleSet_cond_iff.mpr
+      simp; omega
+  · intro ⟨h1, h2⟩
+    let ⟨_, _, hr⟩ := Finset.filter_nonempty_iff.mp (Finset.nonempty_iff_ne_empty.mpr h2)
+    have : i.val > m.val ∨ j.val > n.val := by
+      by_contra!
+      absurd h1
+      apply Finset.nonempty_iff_ne_empty.mp
+      use ⟨⟨⟨0, m.2⟩, ⟨0, n.2⟩⟩, ⟨⟨i.val - 1, by omega⟩, ⟨j.val - 1, by omega⟩⟩, by simp, by simp⟩
+      apply rectangleSet_cond_iff.mpr
+      simp; omega
+    rw [ne_eq, ←PNat.coe_inj]
+    omega
 
 /-- **Proposition 2.1**
     NOTE: The original conditions `i ≤ m`, `j ≤ n`, an `m ≤ n` are not necessary.
@@ -71,13 +82,12 @@ theorem rectangleSet_card_val {m n i j : PNat} :
   rw [←Finset.card_range (m + 1 - i), ←Finset.card_range (n + 1 - j), ←Finset.card_product]
   apply Finset.card_bij (fun r _ => ⟨r.topLeft.row, r.topLeft.col⟩)
   · intro r hr
-    apply Finset.mem_product.mpr
     have := rectangleSet_cond_iff.mp hr
     have : r.topLeft.row.val < m + 1 - i := by omega
     have : r.topLeft.col.val < n + 1 - j := by omega
-    simp only [*, and_self, Finset.mem_range]
+    simp only [Finset.mem_range, and_self, Finset.mem_product.mpr, *]
   · intro r1 hr1 r2 hr2 h3
-    simp only [Prod.mk.injEq] at h3
+    rw [Prod.mk.injEq] at h3
     have := rectangleSet_cond_iff.mp hr1
     have := rectangleSet_cond_iff.mp hr2
     have : _ ∧ _ := ⟨r1.validRow, r1.validCol⟩
@@ -92,10 +102,7 @@ theorem rectangleSet_card_val {m n i j : PNat} :
       ⟨⟨p1, by omega⟩, ⟨p2, by omega⟩⟩,
       ⟨⟨i - 1 + p1, by omega⟩, ⟨j - 1 + p2, by omega⟩⟩,
       by simp, by simp⟩
-    simp only [exists_prop, and_true]
-    apply rectangleSet_cond_iff.mpr
-    simp only
-    omega
+    simp [rectangleSet_cond_iff.mpr, *]
 
 def SpinSet (i j : PNat) (m n : PNat) : Finset (Rectangle m n) :=
   RectangleSet i j m n ∪ RectangleSet j i m n
@@ -121,16 +128,14 @@ lemma validSpins_def2 : validSpins m n =
   intro r
   constructor
   · intro
-    simp
+    simp only [Finset.product_biUnion, Finset.mem_biUnion, Finset.mem_range]
     use r.bottomRight.row.val - r.topLeft.row.val, (by omega)
     use r.bottomRight.col.val - r.topLeft.col.val, (by omega)
-    apply rectangleSet_cond_iff.mpr
-    simp
+    simp [rectangleSet_cond_iff.mpr]
   · intro
     simp [validSpins]
     use r.topLeft, r.bottomRight
-    have : _ ∧ _ := ⟨r.validRow, r.validCol⟩
-    simp_all only [and_self, exists_const]
+    simp_all only [r.validRow, r.validCol, and_self, exists_const]
 
 lemma sum_m_minus_x1.aux1 {a b c : Nat} (h: (a : ℤ) = b - c) : a = b - c := by omega
 
@@ -142,9 +147,9 @@ lemma sum_m_minus_x1 (m : PNat) :
     apply sum_m_minus_x1.aux1
     zify
     convert Finset.sum_sub_distrib
-    · rename_i a b
-      have : a < n := List.mem_range.mp b
-      omega
+    rename_i a b
+    have : a < n := List.mem_range.mp b
+    omega
   rw [this, Nat.sub_mul, Finset.sum_range_id_mul_two, Nat.mul_sub_one, Nat.sub_eq_of_eq_add]
   simp only [sum_const, card_range, smul_eq_mul]
   linarith [Nat.add_sub_of_le <| Nat.le_mul_self n]
@@ -157,8 +162,7 @@ lemma sum_m_minus_x (m : PNat) :
 /-- **Proposition 2.3** -/
 theorem total_valid_spins_card {m n : PNat} :
   (validSpins m n).card = (m * (m + 1) / 2) * (n * (n + 1) / 2) := by
-  rw [validSpins_def2]
-  rw [Finset.card_biUnion]
+  rw [validSpins_def2, Finset.card_biUnion]
   · simp only [rectangleSet_card_val, PNat.mk_coe, Nat.reduceSubDiff]
     rw [Finset.sum_product, ←Finset.sum_mul_sum, sum_m_minus_x m, sum_m_minus_x n]
   · simp only [Finset.mem_product, Finset.mem_range, ne_eq, and_imp, Prod.forall,
@@ -169,4 +173,4 @@ theorem total_valid_spins_card {m n : PNat} :
     by_contra hr2
     have := rectangleSet_cond_iff.mp hr
     have := rectangleSet_cond_iff.mp hr2
-    simp_all
+    simp_all only [PNat.mk_coe, add_left_inj, not_true_eq_false, imp_false]
