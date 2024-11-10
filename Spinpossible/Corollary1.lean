0226c1ca321ca81d4da1b4f389b67729a2ba7c46
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-11-10 01:36:51 -0500

    Add half of corollary 1

---
+++ Spinpossible/Corollary1.lean
@@ -0,0 +1,213 @@
+import Spinpossible.Prop2
+
+def Spin.inv (x : Spin m n) : Spin m n := ⟨x.α.symm, fun i => -x.u (x.α.toFun i)⟩
+
+instance : Inv (Spin m n) := ⟨Spin.inv⟩
+
+theorem Spin.mul_assoc (x y z : Spin m n) : x * y * z = x * (y * z) := by
+  ext
+  · rw [Equiv.ext_iff]; exact fun _ ↦ rfl
+  · funext; exact add_assoc ..
+
+theorem Spin.one_mul (x : Spin m n) : 1 * x = x := by
+  ext
+  · rfl
+  · funext; simp [mul_def]; rfl
+
+theorem Spin.mul_one (x : Spin m n) : x * 1 = x := by
+  ext
+  · rfl
+  · funext; exact ((self_eq_add_right.mpr) rfl).symm
+
+theorem Spin.inv_mul_cancel (x : Spin m n) : x⁻¹ * x = 1 := by
+  ext
+  · rw [Equiv.ext_iff]; exact Equiv.apply_symm_apply _
+  · simp [Inv.inv, Spin.inv, mul_def]; rfl
+
+instance : Group (Spin m n) where
+  mul_assoc := Spin.mul_assoc
+  one_mul := Spin.one_mul
+  mul_one := Spin.mul_one
+  inv_mul_cancel := Spin.inv_mul_cancel
+
+
+
+open scoped CharTwo
+
+lemma feelsomething {l : List (Spin m n)} (h : ∀ w ∈ l, w.α = Equiv.refl _) :
+    l.prod.α = Equiv.refl _  := by
+  have : ∀ j : List (Spin m n), j.prod.α = (j.map (fun x => (x.α : Equiv.Perm _))).reverse.prod := by
+    intro j
+    induction' j with head tail ih
+    · rfl
+    · simp [List.map_cons, List.prod_cons, ih, Spin.mul_def]
+      rfl
+  rw [this l, List.map_eq_map_iff.mpr h]
+  simp only [List.map_const', List.reverse_replicate, List.prod_replicate]
+  have : IsIdempotentElem (Equiv.refl (Fin (↑m * ↑n))) := rfl
+  exact IsIdempotentElem.pow_succ_eq _ this
+
+lemma cmon (a : ZMod 2) : a = 0 ∨ a = 1 := by
+  match a with
+  | 0 => exact Or.inl rfl
+  | 1 => exact Or.inr rfl
+
+lemma sum_eq_sum_take_add_nthLe_add_sum_drop_succ
+  [AddCommMonoid α] (l : List α) (i : ℕ) (h : i < l.length) :
+  l.sum = (l.take i).sum + l[i] + (l.drop (i + 1)).sum := by
+  rw [← List.sum_take_add_sum_drop l (i + 1), List.sum_take_succ]
+
+lemma hou [AddCommMonoid α] (l : List α) (i : Fin l.length) :
+  (l.eraseIdx i).sum + l[i] = l.sum := by
+  have h₁ : l.sum = (l.take i).sum + l[i] + (l.drop (i + 1)).sum :=
+    sum_eq_sum_take_add_nthLe_add_sum_drop_succ l i i.isLt
+  have h₂ : (l.eraseIdx i).sum = (l.take i).sum + (l.drop (i + 1)).sum := by
+    rw [List.eraseIdx_eq_take_drop_succ l i, List.sum_append]
+  rw [h₁, h₂]
+  exact add_right_comm _ _ _
+
+lemma hou2 [AddCommMonoid α] (l : List α) (i : Fin l.length) : l.sum = (l[i] :: l.eraseIdx i).sum := by
+  rw [List.sum_cons, add_comm, hou l i]
+
+example {s : Spin m n} {l k : List (Spin m n)} (hl : l.prod.α = s.α)
+    (hk : k = (List.finRange (↑m * ↑n)).filterMap (
+      fun isp ↦
+      if l.prod.u isp ≠ s.u isp
+      then some ⟨Equiv.refl _, fun j ↦ if isp = j then 1 else 0⟩
+      else none)
+    ) : (l ++ k).prod = s := by
+  simp [List.prod_append, Spin.mul_def]
+  have zx : ∀ w ∈ k, w.α = Equiv.refl _ := by
+    intro w hw
+    simp [hk] at hw
+    aesop
+  have asd : k.prod.α = Equiv.refl _ := feelsomething zx
+  ext
+  · rw [asd]
+    exact hl
+  · funext i
+    simp only [hl, Equiv.refl_symm, Equiv.refl_apply]
+    have bam : k.prod.u i = (k.map (fun x => x.u i)).sum := by
+      clear hk
+      induction' k with head tail ih
+      · simp [List.map_cons, List.prod_cons, Spin.mul_def]
+        rfl
+      · simp [List.map_cons, List.prod_cons, ih, Spin.mul_def]
+        have : ∀ w ∈ tail, w.α = Equiv.refl _ := by
+          intro w hw
+          exact zx _ (List.mem_cons_of_mem head hw)
+        have : tail.prod.α = Equiv.refl _ := feelsomething this
+        simp_all
+    have : l.prod.u i = s.u i ∨ l.prod.u i = s.u i + 1 := by
+      cases cmon (l.prod.u i) <;> cases cmon (s.u i) <;> simp_all
+    rcases this with h2 | h2
+    · have qq : ∀ e ∈ k, e.u i = 0 := by
+        intro e he
+        simp [hk] at he
+        aesop
+      have : k.prod.u i = 0 := by
+        simp [bam, List.map_eq_map_iff.mpr qq]
+      simp_all
+    ·
+      simp_all [-hk]
+      have : (List.map (fun x ↦ x.u i) k).sum = 1 := by
+        let ⟨p1, p2, p3⟩ : ∃! f : Fin k.length, (List.map (fun x ↦ x.u i) k)[f] = 1 := by
+          let x_i_def : Spin m n :=
+            { α := Equiv.refl _, u := fun j ↦ if i = j then 1 else 0 }
+          have x_i_in_k : x_i_def ∈ k := by
+            rw [hk]
+            apply List.mem_filterMap.mpr
+            use ⟨i, by simp⟩
+            simp [h2]
+          have knodup : k.Nodup := by
+            rw [hk]
+            refine List.Nodup.filterMap ?_ ?_
+            · intro a1 a2 s hs1 hs2
+              simp only [ne_eq, ite_not, Option.mem_def, Option.ite_none_left_eq_some,
+                Option.some.injEq] at hs1 hs2
+              obtain ⟨-, right⟩ := hs1
+              obtain ⟨-, right_1⟩ := hs2
+              subst right
+              simp only [Spin.mk.injEq, true_and] at right_1
+              have := congr($right_1 a1)
+              by_contra! h
+              simp [h.symm] at this
+            · exact List.nodup_finRange _
+          have important : ∀ s : Fin k.length, k[s].u i = 1 → k[s] = x_i_def := by
+            intro s hs
+            have : k[s] ∈ k := by simp
+            set hh := k[s]
+            simp [hk] at this
+            obtain ⟨b1, -, b3⟩ := this
+            ext
+            · simp [x_i_def, ←b3]
+            · simp [x_i_def]
+              have := congr(Spin.u $b3)
+              simp only at this
+              have asd : b1 = i := by
+                have := congr(Spin.u $b3)
+                rw [funext_iff] at this
+                have := this i
+                simpa [hs] using this
+              exact asd ▸ this.symm
+          obtain ⟨fi, fi_eq⟩ := List.get_of_mem x_i_in_k
+          use ⟨fi, by simp⟩
+          simp
+          constructor
+          · have : k.get fi = k[fi.val] := rfl
+            rw [←this, fi_eq]
+            simp
+          · intro fi' hfi'
+            by_contra! hg
+            have ee1 : k[fi'] = x_i_def := important fi' hfi'
+            have : ¬ k.Nodup := by
+              refine List.not_nodup_of_get_eq_of_ne k fi fi' ?_ hg.symm
+              simp_all
+            exact this knodup
+        set l3 := List.map (fun x ↦ x.u i) k
+        simp at p2 p3
+        set l3 := List.map (fun x ↦ x.u i) k
+        have : l3.length = k.length := by simp [l3]
+        let p1' : Fin l3.length := ⟨p1.val, by omega⟩
+        have : l3.sum = (l3[p1'] :: l3.eraseIdx p1').sum := hou2 l3 p1'
+        rw [this, List.sum_cons]
+        have sa : l3[p1'] = 1 := by simp_all [l3, p1']
+        set l4 := l3.eraseIdx p1'
+        have : ∀ e ∈ l4, e = 0 := by
+          intro e he
+          let o := List.indexOf e l3
+          have re : o < l3.length := List.indexOf_lt_length.mpr (List.mem_of_mem_eraseIdx he)
+          have : k[o].u i = 0 := by
+            have ew2 : l3[p1] ∉ l4 := by
+              by_contra! h
+              let ⟨y1, y2, y3, y4⟩ := List.mem_eraseIdx_iff_getElem.mp h
+              have ew : y1 ≠ p1 := y3
+              have : k[p1].u i = k[y1].u i := by
+                simp [l3] at y4
+                exact y4.symm
+              have : k[y1].u i = 1 := this ▸ p2
+              have := p3 ⟨y1, by omega⟩ this
+              have := congr(Fin.val $this)
+              simp at this
+              exact ew this
+            have : l3[o] ∈ l4 := by rw [List.getElem_indexOf re]; exact he
+            have qwe : o ≠ p1 := by by_contra! h; simp [h] at this; absurd ew2; exact this
+            by_contra! h
+            have ww : k[o].u i = 1 := by
+              by_contra! h2
+              have := cmon (k[o].u i)
+              simp_all only [Fin.getElem_fin, List.getElem_map, List.sum_cons, ne_eq, or_self]
+            have := p3 ⟨o, by omega⟩
+            simp at this
+            have := this ww
+            absurd qwe
+            rw [←this]
+          have : k[o].u i = e := by
+            have : l3[o] = e := List.getElem_indexOf re
+            have : e = l3[o] := this.symm
+            simp_rw [this] -- why doesn't `rw` work here?
+            simp [l3]
+          simp_all
+        rw [sa, List.sum_eq_zero this, add_zero]
+      rw [this]
+      exact add_eq_of_eq_add_neg rfl
